50feba30a045239b60d4b3fbd073742730316957
d_liyanage <admliyanage@gmail.com>
2020-04-24 21:57:52 +0530

    init

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Chat from '../views/PrivateChat'
 
 Vue.use(VueRouter)
 
@@ -10,6 +11,11 @@ Vue.use(VueRouter)
     name: 'Home',
     component: Home
   },
+    {
+      path: '/chat',
+      name: 'Chat',
+      component: Chat
+    },
   {
     path: '/about',
     name: 'About',
